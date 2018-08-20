f9d8a0e139a934a312566a833bbbd63237ed3784
Tran Vuong Quang <hiimkol@gmail.com>
2018-08-21 00:31:12 +0700

    update assets, alot

---
+++ client/src/app/book.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getBook
+}
