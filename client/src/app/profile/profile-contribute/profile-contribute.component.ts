0c4601e95a47db6f1ceaf11df8df8279fdef0906
tranquanghuy341 <tranquanghuy341@gmail.com>
2018-08-27 09:27:27 +0700

    add form book

--- client/src/app/profile/profile-contribute/profile-contribute.component.ts
+++ client/src/app/profile/profile-contribute/profile-contribute.component.ts
@@ -1,4 +1,6 @@
+
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../../core/models/book.model';
 
 @Component({
   selector: 'app-profile-contribute',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileContributeComponent implements OnInit {
 
+  isAdding = false;
+  buttonLabel: string = 'Contribute';
+
+  bookContributes: Book[] = new Array();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  newContribute(){
+    if (!this.isAdding){
+      this.isAdding = true;
+      this.buttonLabel = 'Back';
+    } else {
+      this.isAdding = false;
+      this.buttonLabel = 'Contribute';
+    }
+    
   }
 
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,19 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent, FooterComponent } from '.';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { FormBookComponent } from './forms/form-book/form-book.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    FormBookComponent
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    FormBookComponent,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class SharedModule { }
