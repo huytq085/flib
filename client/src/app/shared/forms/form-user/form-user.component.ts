6d8ab547e1b415363ce6b59cc380c3605473bc22
tranquanghuy341 <tranquanghuy341@gmail.com>
2018-08-28 13:52:53 +0700

    test

--- client/src/app/shared/forms/form-user/form-user.component.ts
+++ client/src/app/shared/forms/form-user/form-user.component.ts
@@ -1,4 +1,4 @@
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { User, UserService } from '../../../core';
 
@@ -13,36 +13,61 @@ export class FormUserComponent implements OnInit {
   @Output() userEmitter = new EventEmitter<User>();
 
   userForm: FormGroup;
+  isPasswordChange = false;
   isSubmitting = false;
-  
+
   constructor(
     private fb: FormBuilder,
     private userService: UserService
   ) {
     this.userForm = this.fb.group({
-      email: '',
-      password: '',
-      fullName: '',
-      address: '',
-      phone: '',
-      gender: '',
-      identityCard: ''
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')
+      ])],
+      fullName: ['', Validators.compose([
+        Validators.maxLength(25),
+        Validators.minLength(5),
+        Validators.required
+      ])],
+      address: ['', Validators.required],
+      phone: ['', Validators.compose([
+        Validators.maxLength(15),
+        Validators.minLength(8),
+        Validators.pattern('^([0-9]*)$'),
+        Validators.required
+      ])],
+      gender: ['', Validators.required],
+      identityCard: ['', Validators.compose([
+        Validators.maxLength(12),
+        Validators.minLength(9),
+        Validators.pattern('^([0-9]*)$'),
+        Validators.required
+      ])]
     });
   }
 
   ngOnInit() {
-    if (this.user){
+    console.log(this.user);
+    if (this.user.email) {
       this.userForm.patchValue(this.user);
+      this.isPasswordChange = false;
+      if (this.user.password){
+        // this.user.password =  
+      }
+    } else {
+      console.log('else')
+      this.isPasswordChange = true;
     }
   }
 
-  submitForm(){
+  submitForm() {
     // this.isSubmitting = true;
     // update the model
     this.patchUser(this.userForm.value);
     // submit
     console.log(this.user)
-    if (!this.user.id){
+    if (!this.user.id) {
       this.userService.create(this.user).subscribe(
         data => {
           console.log('created');
@@ -60,10 +85,10 @@ export class FormUserComponent implements OnInit {
       )
     }
 
-    
+
   }
 
-  patchUser(values: any){
+  patchUser(values: any) {
     Object.assign(this.user, values);
   }
 
